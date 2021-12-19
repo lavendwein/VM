a6d8141fe5d7aca13f79e861908f76738197f267
Polina Popova <91219397+lavendwein@users.noreply.github.com>
2021-12-19 23:04:09 +0500

    Update vm.js

--- vm.js
+++ vm.js
@@ -7,50 +7,47 @@ let m = 0;
 
 let progText = fs.readFileSync(arg[2]).toString();
 
-ram = progText.split(/\s+/); //разбитие строки по пробелам и переносам строки
-//for (let i = 0; i < ram.length; i++)
-//	console.log(i, ram[i]);
-
+ram = progText.split(/\s+/);
 
 while(ram[ip] != "exit"){
 	switch(ram[ip]){
-		case "input": //ввод данных из консоли
+		case "input":
 			let inputData = require('readline-sync');
 			ram[ram[ip + 1]] = inputData.questionInt("Enter value:");
 			ip += 2;
 			break;
 		
-		case "set": //адрес ячейки, куда сохранять значение, используется для значений, которые не надо вводить
+		case "set": 
 			ram[ram[ip + 1]] = parseInt([ram[ip + 2]]);
 			ip += 3;
 			break;
 		
-		case "add": //+
+		case "add":
 			ram[ram[ip + 3]] = parseInt(ram[ram[ip + 1]]) + parseInt(ram[ram[ip + 2]]);
 			//console.log(ram[ram[ip + 3]]);
 			ip += 4;
 			break;
 		
-		case "mul": //*
+		case "mul": 
 			ram[ram[ip + 3]] = parseInt(ram[ram[ip + 1]]) * parseInt(ram[ram[ip + 2]]);
 			ip += 4;
 			break;
 		
-		case "del":	//:
+		case "del":
 			ram[ram[ip + 3]] = parseInt(ram[ram[ip + 1]]) / parseInt(ram[ram[ip + 2]]);
 			ip += 4;
 			break;
 		
-		case "min": //-
+		case "min": 
 			ram[ram[ip + 3]] = parseInt(ram[ram[ip + 1]]) - parseInt(ram[ram[ip + 2]]);
 			ip += 4;
 			break;
 		
-		case "mark": //метка
+		case "mark": 
 			ip += 2;
 			break;
 			
-		case "compare": //сравнение
+		case "compare": 
 			let value = parseInt(ram[ram[ip + 1]]) - parseInt(ram[ram[ip + 2]]); 
 			if (value == 0){
 				m = 0;
@@ -65,9 +62,7 @@ while(ram[ip] != "exit"){
 			break;
 			
 		case "jump":
-			//console.log(ram[ip + 1].slice(0, ram[ip + 1].length));
 			ip = findMark(ram, ram[ip + 1]) + 1;
-			//console.log(ip);
 			break;
 
 		case "jumpe": //jump equal
@@ -81,7 +76,6 @@ while(ram[ip] != "exit"){
 		case "jumpl": //jump less
 			if (m == -1){
 				ip = findMark(ram, ram[ip + 1]) + 1;
-				//console.log(ip);
 			}else{
 				ip += 2;
 			}
@@ -90,7 +84,6 @@ while(ram[ip] != "exit"){
 		case "jumpm": //jump more
 			if (m == 1){
 				ip = findMark(ram, ram[ip + 1]) + 1;
-				//console.log(ip);
 			}else{
 				ip += 2;
 			}
@@ -108,7 +101,6 @@ while(ram[ip] != "exit"){
 function findMark(ram, nameMark){ //findMark(ip)
 	for (let j = 0; j < ram.length; j++){
 		if (ram[j] === nameMark.slice(0, nameMark.length) && ram[j - 1] === "mark"){
-			//console.log(j);
 			return j;
 		}
 	}
